a3d0df955a4d3abf665380c88063025d4a9125c5
Овчинников <156998038+Ovchinnikov01@users.noreply.github.com>
2024-04-30 23:54:54 +0300

    Github

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js",
+    './node_modules/flowbite/**/*.js',
+    
+    
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'smoky': '#f2f3f5',
+        'white-2': '#fdfdfd',
+        'white-3': '#EDF2F7',
+      },
+      spacing: {
+        '1050px': '1050px',
+      },
+    }
+    },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
